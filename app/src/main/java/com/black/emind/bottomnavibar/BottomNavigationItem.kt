354ed065f882dd16588eaa0b8ebb1b8c78127161
iUser <author-main@mail.ru>
2021-11-12 20:37:18 +0300

    08.11.2021

--- app/src/main/java/com/black/emind/bottomnavibar/BottomNavigationItem.kt
+++ app/src/main/java/com/black/emind/bottomnavibar/BottomNavigationItem.kt
@@ -5,13 +5,37 @@ import com.black.emind.R
 
 enum class Route {
     SEARCH,
+    NOTE, TASK, DOCUMENT,
     INSERT,
     FAVORITE,
     MENU
 }
 
+/*
+ * object Screen - активный экран:
+ * NOTE     - заметки
+ * TASK     - список задач
+ * DOCUMENT - документы
+ */
 sealed class BottomNavigationItem(var route: Route, @DrawableRes var icon: Int){
+    fun setScreenRoute(route: Route) {
+        this.route = route
+        icon = when (route) {
+            Route.TASK -> R.drawable.task
+            Route.DOCUMENT -> R.drawable.document
+            else -> R.drawable.note
+        }
+    }
+    fun isScreenRoute(route: Route) =
+        route == Route.NOTE || route == Route.TASK || route == Route.DOCUMENT
     object Search:   BottomNavigationItem(Route.SEARCH,     R.drawable.search)
+    /*
+    * object Screen - активный экран:
+    * NOTE     - заметки
+    * TASK     - список задач
+    * DOCUMENT - документы
+    */
+    object Screen:   BottomNavigationItem(Route.NOTE,       R.drawable.note)
     object Insert:   BottomNavigationItem(Route.INSERT,     R.drawable.insert)
     object Favorite: BottomNavigationItem(Route.FAVORITE,   R.drawable.favorite)
     object Menu:     BottomNavigationItem(Route.MENU,       R.drawable.menu)
