a5a21768fc902e12afe376653ca9a5e27b4955cb
iUser <author-main@mail.ru>
2021-11-13 10:20:49 +0300

    08.11.2021

--- app/src/main/java/com/black/emind/bottomnavibar/BottomNavigationItem.kt
+++ app/src/main/java/com/black/emind/bottomnavibar/BottomNavigationItem.kt
@@ -11,27 +11,36 @@ enum class Route {
     MENU
 }
 
-sealed class BottomNavigationItem(var route: Route, @DrawableRes var icon: Int){
+enum class ItemIcon(@DrawableRes val icon: Int, @DrawableRes val iconSelect: Int) {
+    SEARCH(R.drawable.search, R.drawable.search_on),
+    INSERT(R.drawable.insert, R.drawable.insert_on),
+    FAVORITE(R.drawable.favorite, R.drawable.favorite_on),
+    MENU(R.drawable.menu, R.drawable.menu_on),
+    NOTE(R.drawable.note, R.drawable.note_on),
+    TASK(R.drawable.task, R.drawable.task_on),
+    DOCUMENT(R.drawable.document, R.drawable.document_on),
+}
 
-    object Search:   BottomNavigationItem(Route.SEARCH,     R.drawable.search)
+sealed class BottomNavigationItem(var route: Route, var icon: ItemIcon){
+    object Search:   BottomNavigationItem(Route.SEARCH,     ItemIcon.SEARCH)
     /*
     * object Screen - активный экран:
     * NOTE     - заметки
     * TASK     - список задач
     * DOCUMENT - документы
     */
-    object Screen:   BottomNavigationItem(Route.NOTE, R.drawable.note){
+    object Screen:   BottomNavigationItem(Route.NOTE, ItemIcon.NOTE){
         @JvmName("setRoute1")
         fun setScreen(route: Route) {
             this.route = route
             icon = when (route) {
-                Route.TASK -> R.drawable.task
-                Route.DOCUMENT -> R.drawable.document
-                else -> R.drawable.note
+                Route.TASK -> ItemIcon.TASK
+                Route.DOCUMENT -> ItemIcon.DOCUMENT
+                else -> ItemIcon.NOTE
             }
         }
     }
-    object Insert:   BottomNavigationItem(Route.INSERT,     R.drawable.insert)
-    object Favorite: BottomNavigationItem(Route.FAVORITE,   R.drawable.favorite)
-    object Menu:     BottomNavigationItem(Route.MENU,       R.drawable.menu)
+    object Insert:   BottomNavigationItem(Route.INSERT,     ItemIcon.INSERT)
+    object Favorite: BottomNavigationItem(Route.FAVORITE,   ItemIcon.FAVORITE)
+    object Menu:     BottomNavigationItem(Route.MENU,       ItemIcon.MENU)
 }
