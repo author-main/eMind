84c95f52d3d557b155b5049f4fcc61a8096492b8
iUser <author-main@mail.ru>
2021-11-15 16:45:20 +0300

    08.11.2021

--- app/src/main/java/com/black/emind/bottomnavibar/BottomNavigationItem.kt
+++ app/src/main/java/com/black/emind/bottomnavibar/BottomNavigationItem.kt
@@ -94,6 +94,21 @@ fun BottomNavigationBar(navController: NavController) {
                 selected = false,
                 onClick = {
                     log(item.route.id)
+                    navController.navigate(item.route.id) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        // Avoid multiple copies of the same destination when
+                        // reselecting the same item
+                        launchSingleTop = true
+                        // Restore state when reselecting a previously selected item
+                        restoreState = true
+                    }
                 }
             )
         }
