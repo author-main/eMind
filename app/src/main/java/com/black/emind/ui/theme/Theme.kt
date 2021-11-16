3f5b97bd20d4a725d270235ebe93551d57d8977b
iUser <author-main@mail.ru>
2021-11-16 13:09:23 +0300

    15.11.2021

--- app/src/main/java/com/black/emind/ui/theme/Theme.kt
+++ app/src/main/java/com/black/emind/ui/theme/Theme.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -14,21 +16,34 @@ private val DarkColorPalette = darkColors(
 
 private val LightColorPalette = lightColors(
     primary = DarkGray,//Purple500,
-    primaryVariant = Purple700,
-    secondary = Orange
+    primaryVariant = DarkGray,
+    secondary = Orange,
 
-    /* Other default colors to override
+/*
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+*/
 )
 
 @Composable
 fun EMindTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+
+    val systemUiController = rememberSystemUiController()
+    if(darkTheme){
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent
+        )
+    }else{
+        systemUiController.setSystemBarsColor(
+            color = Color.White
+        )
+    }
+
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
