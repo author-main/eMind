bb5452928d05bbecb34c1f7ee8a30f99e57e08d9
iUser <author-main@mail.ru>
2021-12-13 21:29:41 +0300

    11.12.2021

--- app/src/main/java/com/black/emind/screenObjComposable/ColorDialog.kt
+++ app/src/main/java/com/black/emind/screenObjComposable/ColorDialog.kt
@@ -13,17 +13,14 @@ import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.black.emind.R
-import com.black.emind.log
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
