feecc64aaa96db06ed1e923f78513a84acf2c1c7
iUser <author-main@mail.ru>
2022-03-01 10:43:17 +0300

    01.03.2022

--- app/src/main/java/com/black/emind/screenObjComposable/enumScreen/Dialog.kt
+++ app/src/main/java/com/black/emind/screenObjComposable/enumScreen/Dialog.kt
@@ -3,15 +3,13 @@ package com.black.emind.screenObjComposable.enumScreen
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
 import com.black.emind.DEFAULT_FONTCOLOR
-import com.black.emind.screenObjComposable.COLOR_OBJ
 
 sealed class Dialog {
     var onPositiveClick: ((color: androidx.compose.ui.graphics.Color) -> Unit)? = null
-    object None         : Dialog()
-    object Color        : Dialog() {
-        var startColor  = DEFAULT_FONTCOLOR
+    object None                : Dialog()
+    object Color               : Dialog() {
+        var startColor: androidx.compose.ui.graphics.Color = DEFAULT_FONTCOLOR
     }
     object InsertButtons: Dialog()
 }
